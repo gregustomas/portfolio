1cbd2ddc85d68e63be8c52f7de4b0fe6528e603b
Tomáš Greguš <gregustomas@seznam.cz>
2026-02-08 19:11:07 +0100

    feat: add content collections

---
+++ src/content/config.ts
@@ -0,0 +1,41 @@
+import { defineCollection, z } from "astro:content";
+import { date } from "astro:schema";
+
+const projectsCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      project: z.string(),
+      text: z.string(),
+      url: z.string().url(),
+      clr: z.string(),
+      img: image(),
+    }),
+});
+
+const skillsCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      skill: z.string(),
+      text: z.string(),
+      img: image(),
+    }),
+});
+
+const highlightCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      text: z.string(),
+      img: image(),
+      date: date(),
+    }),
+});
+
+export const collections = {
+  'projects': projectsCollection,
+  'skills': skillsCollection,
+  'highlight': highlightCollection,
+};
